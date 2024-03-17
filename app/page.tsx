acc973cf89f322ad34b5383b4ce05ca68b28972a
Mateusz Łuczak <luczakmmateusz@gmail.com>
2024-03-17 18:47:26 +0100

    feat: chapter5

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from './ui/fonts';
-import Image from 'next/image';v
+import Image from 'next/image';
 
 export default function Page() {
   return (
